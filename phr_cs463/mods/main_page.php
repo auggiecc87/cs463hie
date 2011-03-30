fa6361ade04665859d21ac2e6a59f3ae4834c11e
liquid <liquid@apollo.localdomain>
2011-03-30 03:47:19 -0500

    cosmetic additions

--- phr_cs463/mods/main_page.php
+++ phr_cs463/mods/main_page.php
@@ -1,5 +1,11 @@
 <?php
 echo "<div class='tempcolor'>";
+if($_SESSION['loggedin']!= 1)
+{
     echo "<p>Welcome,<br/>Please <a href='./index.php?disp=login'>login</a><p>";
+}else
+{
+echo "You are currently logged in";
+}
 echo "</div>";
 ?>
