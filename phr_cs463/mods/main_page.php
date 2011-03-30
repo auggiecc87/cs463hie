2de120d13881fdf2c3bf45cb4697ea65ea6fc187
liquid <liquid@apollo.localdomain>
2011-03-30 00:56:26 -0500

    removed unecessary files and updated messages

--- phr_cs463/mods/main_page.php
+++ phr_cs463/mods/main_page.php
@@ -1,14 +1,5 @@
 <?php
 echo "<div class='tempcolor'>";
-if ($_SESSION['VALID'] == 1)
-{
-$user_name = $_SESSION['USERNAME'];
-    echo "<p>Welcome Home $user_name<p>";
-
-}else
-{
-    echo "<p>Welcome Home,<br/>Please <a href='./index.php?disp=login'>login</a> to control the energy settings<p>";
-
-}
+    echo "<p>Welcome,<br/>Please <a href='./index.php?disp=login'>login</a><p>";
 echo "</div>";
 ?>
